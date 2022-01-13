2fbde9b2bd66bbde48cb9664f2f44ec4c5a0e24b
gyektai-brown <gideon_yektai@brown.edu>
2022-01-12 22:20:35 -0500

    Refactor into a smart contract rather than smart signature for a more elagant solution. Also the previous one was broken because I never did any rekey checks or the likes because I'm not familiar with smart sigs

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # algo-burner
-Burner account for Algorand's blockchain
+Burner address for Algorand's blockchain
 
 Apparently it was a problem that people couldn't verifiably burn ASAs, and the current best way I've seen was sending the ASA to an account that you create and opt in and then rekey that account to a verifiably unknown account. I thought that was clunky so I made another way.
 
-Have the contract opt in to an asset, and then you can send as many of that asset to the account and completely lose access to them! Gone for good, so be careful!
+Send an application call to the smart contract at appID 541574524 with the asset you want it to opt into being the firt entry in the assets array. Then send the ASAs you want to burn to the application address at AEI5EMQIYQTRTDV4BCZSVNQKO3LYOBKJAL67YY4OO5WJGC443NOYDDPC2Y
 
-The account address is JBPZI53GLJ4FW3KHKHC6PDWHRQARLRGC7DM33IT7XODFMIFNJW3CTUAIEM and I initially funded it at txn FIWRGYKBNL6RWWVI3Z5MPTNOKZQU25CQIVPRUIAVYHLH57XXD3TQ
-
-I used the compiled version of this exact file in this repo for everything
+I used the compiled version of the exact file in this repo for everything, if you'd like to verify. You're also welcome to create this application yourself and just do that
--- contract.py
+++ contract.py
@@ -3,13 +3,43 @@ import os
 
 def approval_program():
 
-    # the account can only transfer assets in amounts of 0
-    # so it can opt in, but never do anything with what it receives
-    program = And(
-        Txn.type_enum() == TxnType.AssetTransfer,
-        Txn.asset_amount() == Int(0)
+    # to check if the accounts already opted into the asset
+    asset_balance = AssetHolding.balance(Txn.accounts[0], Txn.assets[0])
+    opted = Seq(
+        asset_balance,
+        asset_balance.hasValue()
     )
 
+    # the account can only opt in to assets on an application call
+    # but won't ever be able to send them
+    handle_noop = Seq(
+        Assert(opted == Int(0)),
+        InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields({
+                TxnField.type_enum: TxnType.AssetTransfer,
+                TxnField.xfer_asset: Txn.assets[0],
+                TxnField.receiver: Global.current_application_address(),
+                TxnField.asset_amount: Int(0),
+            }),
+        InnerTxnBuilder.Submit(),
+        Int(1)
+    )
+
+    # can't update, optin, closeout, delete
+    program = Cond(
+        [Txn.application_id() == Int(0), Approve()],
+        [Txn.on_completion() == OnComplete.OptIn, Reject()],
+        [Txn.on_completion() == OnComplete.CloseOut, Reject()],
+        [Txn.on_completion() == OnComplete.UpdateApplication, Reject()],
+        [Txn.on_completion() == OnComplete.DeleteApplication, Reject()],
+        [Txn.on_completion() == OnComplete.NoOp, Return(handle_noop)]
+    )
+
+    return compileTeal(program, mode=Mode.Application, version=5)
+
+# shouldn't be necessary since there's no opting in, but alway approve it
+def clear_state_program():
+    program = Return(Int(1))
     return compileTeal(program, mode=Mode.Application, version=5)
 
 if __name__ == "__main__":
@@ -17,3 +47,6 @@ if __name__ == "__main__":
 
     with open(os.path.join(path,"burnerProgram.teal"), "w") as f:
         f.write(approval_program())
+    
+    with open(os.path.join(path,"clearBurnerProgram.teal"), "w") as f:
+        f.write(clear_state_program())
